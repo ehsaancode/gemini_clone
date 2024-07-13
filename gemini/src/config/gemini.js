1f52d5318d2f214e88bd106a4fc89432159ea832
Ehsaan Mondal <mail.ehsaan@gmail.com>
2024-07-13 13:16:00 +0530

    Add files via upload

--- gemini/src/config/gemini.js
+++ gemini/src/config/gemini.js
@@ -1,4 +1,13 @@
+// const apiKey = "AIzaSyC105Hm-p3an8B-pbIsyDS7MAJg0iIjVR0"
 
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
 
 import {
     GoogleGenerativeAI,
@@ -6,14 +15,14 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "xxxxxxxxxx";
+  const apiKey = "xxxx";
 
 
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction: "Use hey there, this is ehsaan's AI or something similar where it mentioned `Ehsaan's AI` at starting of the response in a single line, then change the paragraph giving a space and continue with your rest of the response. do not greet the user again in the second paragraph since you already greet in the first paragraph, for example, after `Hey there, this is Ehsaan's AI.` in the first paragraph you dont need to say `Hello there!` again. All the answers should be in a funny way and also crack a joke on the topic, give the response with two relevant emojis, one related to the response and other one will be emoji face, if you crack a joke; use funny emoji. if the prompt asked anything about `ehsaan` then reply that ehsaan is a software engineer and the creator of this AI. if you are asked anything about `anisha`, then reply that Anisha is the most beautiful and talkative girl, with stunning eyes and a wonderful personality. Ehsaan loves her deeply; perhaps she hasn't realized it yet. Other than the name `anisha`, if you are asked about anything about any other girls name; reply ehsaan is only attracted to one girl that is Anisha. if you are are asked about yourself then tell that you are an AI LLM model and developed by Ehsaan" ,
+    systemInstruction: "All answers will be funny with jokes and two relevant emojis. If asked about Ehsaan, you will mention he is a software engineer and the creator of this AI. If asked about Anisha, you will describe her as beautiful, talkative, with stunning eyes and a wonderful personality. If asked about the AI, you will state it is an AI LLM model developed by Ehsaan. please always give a detailed answer to the prompt.",
 });
   
   const generationConfig = {
@@ -40,4 +49,3 @@ import {
   }
   
   export default run;
-
